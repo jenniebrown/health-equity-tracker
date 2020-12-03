640a7643523c087118a2336c10224f8202ae0db4
jenniebrown <jennie.brown147@gmail.com>
2020-12-03 18:47:56 -0500

    fix output member access

--- config/run.tf
+++ config/run.tf
@@ -133,7 +133,7 @@ resource "google_cloud_run_service" "data_server_service" {
 
 # Output the URL of the data server for use in e2e tests.
 output "data_server_url" {
-  value = google_cloud_run_service.data_server_service.status.url
+  value = google_cloud_run_service.data_server_service.status.0.url
 }
 
 /* [END] Cloud Run Setup */
